1b36b11a197c6428aae6e8fb5940b772f2547b5b
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-11-18 18:24:05 -0500

    Add message board controller and tests

---
+++ app/controllers/message_boards_controller.rb
@@ -0,0 +1,10 @@
+class MessageBoardsController < ApplicationController
+  # protect_from_forgery :except => [:create]
+  def index
+    binding.pry
+  end
+
+  def create
+    binding.pry
+  end
+end
