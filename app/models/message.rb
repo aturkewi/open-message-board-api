01aefa262628c51431b6a5629746b0fa8c8cd033
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-11-19 10:22:23 -0500

    Add missing comma to message validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
-  validates :message_board_id, :content, presence: true
+  validates :message_board_id, :content, :user_name, presence: true
   
   belongs_to :message_board
 end
