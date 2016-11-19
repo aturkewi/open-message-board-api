5e11447b312cf4a5d098d3c2bea7a27f53fb9059
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-11-19 10:23:07 -0500

    Create messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,35 @@
 class MessagesController < ApplicationController
+  before_action :get_message_board
+  def index
+    if @mb
+      render json: @mb.messages
+    else
+      render json: {
+        error: 'that message board does not yet exist',
+        status: 400
+      }, status:400
+    end
+  end
+  
+  def create
+    message = @mb.messages.create(message_params)
+    if message.save
+      redirect_to "/message_boards/#{@mb.name}/messages"
+    else
+      render json: {
+        error: message.errors.full_messages,
+        status: 400
+      }, status: 400
+    end
+  end
+  
+  private
+  
+  def get_message_board
+    @mb = MessageBoard.find_by(name: params[:board_name])
+  end
+  
+  def message_params
+    params.require(:message).permit(:user_name, :content)
+  end
 end
